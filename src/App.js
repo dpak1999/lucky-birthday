cc1ae48bd6adc761229f755820a3961655ca0c38
dpak1999 <deepakkumardash313@gmail.com>
2021-12-05 12:31:45 +0530

    add : input fields

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 /** @format */
 
+import InputField from './components/InputField';
+import SocialLinks from './components/SocialLinks';
+
 const App = () => {
   return (
     <div className="min-h-screen bg-purple-200">
       <h1 className="text-2xl font-bold font-mono text-center pt-4 underline">
         Is your Birthday lucky
       </h1>
+      <div className="text-center">
+        <InputField />
+        <SocialLinks />
+      </div>
     </div>
   );
 };
--- src/components/InputField.js
+++ src/components/InputField.js
@@ -1,7 +1,51 @@
 /** @format */
 
+import { useState } from 'react';
+
 const InputField = () => {
-  return <div>Input</div>;
+  const [date, setDate] = useState('');
+  const [number, setNumber] = useState('');
+
+  return (
+    <div className="mt-10">
+      <div>
+        <label htmlFor="date" className="text-xl font-mono">
+          Choose your birthdate
+        </label>
+        <br />
+        <input
+          type="date"
+          name="date"
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+          className="w-96 p-2 cursor-pointer outline-none border border-purple-700 rounded-md text-gray-500 mt-2"
+        />
+      </div>
+
+      <div className="mt-6">
+        <label htmlFor="number" className="text-xl font-mono">
+          What is you lucky number
+        </label>
+        <br />
+        <input
+          type="number"
+          name="number"
+          value={number}
+          onChange={(e) => setNumber(e.target.value)}
+          className="w-96 p-2 cursor-pointer outline-none border border-purple-700 rounded-md text-gray-500 mt-2"
+        />
+      </div>
+
+      <button
+        onClick={() => {
+          console.log(typeof date, number);
+        }}
+        className="mt-4 border border-purple-500 px-5 py-2 rounded-md cursor-pointer hover:text-white hover:bg-purple-500"
+      >
+        Check
+      </button>
+    </div>
+  );
 };
 
 export default InputField;
