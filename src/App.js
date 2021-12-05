342c6ffe9b9efa6932740a4f2f2d2814b9434cb6
dpak1999 <deepakkumardash313@gmail.com>
2021-12-05 13:22:55 +0530

    add : footer links

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import SocialLinks from './components/SocialLinks';
 
 const App = () => {
   return (
-    <div className="min-h-screen bg-purple-200">
+    <div className="min-h-screen bg-purple-200 ">
       <h1 className="text-2xl font-bold font-mono text-center pt-4 underline">
         Is your Birthday lucky
       </h1>
--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -1,7 +1,38 @@
 /** @format */
 
 const SocialLinks = () => {
-  return <div>Links</div>;
+  const socialLinks = [
+    {
+      link: 'https://github.com/dpak1999',
+      icon: 'https://img.icons8.com/material-outlined/96/000000/github.png',
+      name: 'github',
+    },
+    {
+      link: 'https://www.linkedin.com/in/deepak-kumar-dash-14a293176/',
+      icon: 'https://img.icons8.com/material-rounded/96/000000/linkedin--v1.png',
+      name: 'linkedin',
+    },
+    {
+      link: 'https://www.facebook.com/deepak.dash.529/',
+      icon: 'https://img.icons8.com/ios-glyphs/90/000000/facebook.png',
+      name: 'facebook',
+    },
+  ];
+
+  return (
+    <>
+      <div className="flex flex-row justify-center text-sm mt-96">
+        {socialLinks.map((item) => (
+          <a href={item.link} target="_blank" rel="noreferrer">
+            <img src={item.icon} alt={item.name} className="h-12" />
+          </a>
+        ))}
+      </div>
+      <span className="font-mono mt-2 text-lg">
+        &copy; 2021 | Deepak kumar dash | Privacy Policy
+      </span>
+    </>
+  );
 };
 
 export default SocialLinks;
